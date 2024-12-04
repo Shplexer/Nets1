21e7d55ed56f1bc254d707d88a4b0f1e34a73033
Дыня Орехов <danaran319@gmail.com>
2024-12-05 01:34:09 +0300

    done

--- Program.cs
+++ Program.cs
@@ -11,53 +11,105 @@ class Program {
     //static private BitArray 
     static private BitArray VControlSumBits = new BitArray(0);
     static private BitArray HControlSumBits = new BitArray(0);
-    static private ushort poly = 0x0805;
+    static private ushort crc;
+    static private string filePath = "";
+    static private readonly byte[] testInitialBytes1 = [49, 50, 51, 52, 53, 54, 55, 56, 57];
+    static private readonly ushort testResultBytes1 = 0x082D;
+    static private readonly byte[] testInitialBytes2 = [57, 56, 55, 54, 53, 52, 51, 50, 49];
+    static private readonly ushort testResultBytes2 = 0x1F74;
     static void Main() {
-        string filePath = "file1.txt";
-        byte[] incomingMessage = ReadFile(filePath);
-        foreach (byte b in incomingMessage) {
-            Console.WriteLine($"0x{b:X4} : {b}");
+        //string filePath = "file1.txt";
+        if (Test()) {
+            Console.WriteLine("Тесты CRC прошли успешно.");
+            filePath = GetExistingFile();
+            byte[] incomingMessage = ReadFile();
+            bitMatrix = GetBitMatrix((byte[])incomingMessage.Clone());
+            parityControlMatrix = ParityControl();
+            verticalAndHorizontalParityControlMatrix = VerticalAndHorizontalParityControl();
+            crc = CalculateCRC((byte[])incomingMessage.Clone());
+            //crcBits = CRC(newBytes);
+            //PrintBinary(crc);
+            PrintBytes((byte[])incomingMessage.Clone(), "Исходные данные");
+            PrintMatrix(parityControlMatrix, "Контроль по паритету");
+            PrintMatrix(verticalAndHorizontalParityControlMatrix, "Вертикальный и горизонтальный контроль по паритету");
+            Console.WriteLine($"========CRC========");
+            Console.WriteLine($"CRC: 0x{crc:X4}");
+            Console.WriteLine($"============================");
+            Console.WriteLine("");
+
+            SaveToFile();
         }
-        byte[] convertedMessage = incomingMessage;
-        //byte[] convertedMessage = ConvertToCorrectEncoding(incomingMessage, 1251, 866);
-        bitMatrix = GetBitMatrix((byte[])convertedMessage.Clone());
-
-        Console.WriteLine("=1===");
-        foreach (byte b in convertedMessage) {
-            Console.WriteLine($"0x{b:X4} : {b}");
+        else {
+            Console.WriteLine("Тесты CRC не пройдены. Попробуйте снова.");
         }
-        Console.WriteLine("==1==");
-
-
-        parityControlMatrix = ParityControl(bitMatrix);
-        verticalAndHorizontalParityControlMatrix = VerticalAndHorizontalParityControl(bitMatrix);
-        ushort crc = CalculateCRC((byte[])convertedMessage.Clone());
-        //crcBits = CRC(newBytes);
-        PrintBinary(crc);
-        Console.WriteLine($"0x{crc:X4}");
-        PrintMatrix(bitMatrix, "Исходные данные");
-        PrintMatrix(parityControlMatrix, "Контроль по паритету");
-        PrintMatrix(verticalAndHorizontalParityControlMatrix, "Вертикальный и горизонтальный контроль по паритету");
-        
-        SaveToFile(filePath);
         //SaveToFile(newBytes, filePath);
     }
-    private static byte[] ReadFile(string filePath) {
+    private static bool Test() {
+        bool testResult = true;
+        if (CalculateCRC(testInitialBytes1) != testResultBytes1) {
+            PrintBinary(CalculateCRC(testInitialBytes1));
+            PrintBinary(testResultBytes1);
+            Console.WriteLine("Тест 1 не пройден.");
+            testResult = false;
+        }
+        if (CalculateCRC(testInitialBytes2) != testResultBytes2) {
+            PrintBinary(CalculateCRC(testInitialBytes2));
+            PrintBinary(testResultBytes2);
+            Console.WriteLine("Тест 2 не пройден.");
+            testResult = false;
+        }
 
-        byte[] fileBytes = File.ReadAllBytes(filePath);
-        return fileBytes;
+        return testResult;
+    } 
+    private static string GetExistingFile() {
+        while (true) {
+            Console.Write("Введите путь к файлу: ");
+            string fileName = Console.ReadLine();
+
+            if (File.Exists(fileName)) {
+                //Console.WriteLine($"File '{fileName}' exists.");
+                return fileName;
+            }
+            else {
+                Console.WriteLine($"Файл '{fileName}' не существует. Попробуйте снова.");
+            }
+        }
     }
-    private static void SaveToFile(string filePath) {
+    //private static byte[] ReadFile() {
+    //    byte[] bytes = new byte[new FileInfo(filePath).Length];
+    //    int iter = 0;
+    //    using (var stream = File.Open(filePath, FileMode.Open)) {
+    //        using (var reader = new BinaryReader(stream)) {
+    //            while (reader.PeekChar() > -1) {
+    //                bytes[iter] = reader.ReadByte();
+    //                iter++;
+    //            }
+    //        }
+    //    }
+    //    //byte[] fileBytes = File.ReadAllBytes(filePath);
+    //    return bytes;
+    //}
+    private static byte[] ReadFile() {
+        using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read)) {
+            byte[] buffer = new byte[1024]; // Use a reasonable buffer size
+            using (var memoryStream = new MemoryStream()) {
+                int bytesRead;
+                while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0) {
+                    memoryStream.Write(buffer, 0, bytesRead);
+                }
+                return memoryStream.ToArray();
+            }
+        }
+    }
+    private static void SaveToFile() {
         filePath = filePath.Substring(0, filePath.Length - 4) + ".ccs";
         Console.WriteLine("Сохраняются байты: ");
         List<BitArray> bitArrays = new List<BitArray>() { VControlSumBits, HControlSumBits };
-        List<String> names = ["Вертикальная контрольная сумма", "Горизонтальная контрольная сумма"];
+        List<String> names = ["Вертикальная контрольная сумма", "Горизонтальная контрольная сумма", "CRC"];
         int index = 0;
-        using (StreamWriter writer = new StreamWriter(filePath)) {
+        using (StreamWriter writer = new StreamWriter(filePath, false, Encoding.UTF8)) {
             foreach (BitArray bitArray in bitArrays) {
                 string line = $"{names[index]}: \n";
-                Console.WriteLine($"{names[index]}: ");
-                PrintBitArray(bitArray, names[index]);
                 byte[] bytes = new byte[(bitArray.Length + 7) / 8];
                 //if (index != 2) {
                 //}
@@ -66,37 +118,44 @@ class Program {
                 for (int i = 0; i < reversedBytes.Length; i++) {
                     reversedBytes[i] = Mirror8Bits(bytes[i]);
                 }
+                PrintBytes(reversedBytes, names[index]);
                 foreach (byte b in reversedBytes) {
                     byte byteToSave = b;
                     char c = (char)byteToSave;
-                    Console.Write($"{byteToSave} : 0x{byteToSave:X2} : {c} : {Convert.ToString(byteToSave, 2).PadLeft(8, '0')}");
-                    line += $"{c}";
-                    Console.WriteLine("");
+                    line += $"{c} -> {byteToSave}\n";
                 }
-                //line += "\n=============";
                 writer.WriteLine(line);
                 index++;
             }
+            byte[] crcBytes = BitConverter.GetBytes(crc);
+            string crcLine = $"{names[index]}: \n";
+            Array.Reverse(crcBytes);
+            PrintBytes(crcBytes, names[index]);
+            foreach (byte byteToSave in crcBytes) {
+                char c = (char)byteToSave;
+                crcLine += $"{c} -> {byteToSave}\n";
+            }
+            writer.WriteLine(crcLine);
         }
 
         //File.WriteAllBytes(filePath, bytesToSave);
         Console.WriteLine($"\nРезультат сохранен в файл '{filePath}'");
     }
 
-    private static void PrintBitArray(BitArray bitArray, string message) {
-        Console.WriteLine($"========{message}========");
-        foreach (bool bit in bitArray) {
-            Console.Write(bit ? "1" : "0");
-        }
-        Console.WriteLine("");
-    }
     private static void PrintMatrix(List<BitArray> matrix, string message) {
         Console.WriteLine($"========{message}========");
         foreach (BitArray bitArray in matrix) {
+        Console.Write("0b");
             foreach (bool bit in bitArray) {
                 Console.Write(bit ? "1" : "0");
             }
             Console.WriteLine("");
+        }
+    }
+    private static void PrintBytes(byte[] bytes, string message) {
+        Console.WriteLine($"========{message}========");
+        foreach (byte b in bytes) {
+            Console.WriteLine($"char: {(char)b} -> {b} -> 0x{b:X4} -> 0b{Convert.ToString(b, 2)}");
         }
     }
     private static List<BitArray> GetBitMatrix(byte[] bytes) {
@@ -111,19 +170,10 @@ class Program {
         }
         return bitMatrices;
     }
-    private static byte[] ConvertToCorrectEncoding(byte[] initialBytes, int encodingCodePage, int finalCodePage) {
-        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-        Encoding initialEncoding = Encoding.GetEncoding(encodingCodePage);
-        string decodedString = initialEncoding.GetString(initialBytes);
-        Encoding finalEncoding = Encoding.GetEncoding(finalCodePage);
-        byte[] convertedBytes = finalEncoding.GetBytes(decodedString);
-        return convertedBytes;
-    }
-
-    private static List<BitArray> ParityControl(List<BitArray> bitMatrix) {
+    private static List<BitArray> ParityControl() {
         //контроль по паритету
         List<BitArray> resultBytes = new List<BitArray>();
-        VControlSumBits = GetControlSumVertical(bitMatrix);
+        VControlSumBits = GetControlSumVertical();
         for (int i = 0; i < bitMatrix.Count; i++) {
             BitArray resultByte = new BitArray(bitMatrix[i].Length + 1);
             for (int j = 0; j < bitMatrix[i].Length; j++) {
@@ -134,7 +184,7 @@ class Program {
         }
         return resultBytes;
     }
-    private static BitArray GetControlSumVertical(List<BitArray> bitMatrix) {
+    private static BitArray GetControlSumVertical() {
         BitArray controlSumBits = new(bitMatrix.Count);
         //Проходим по каждому байту
         for (int i = 0; i < bitMatrix.Count; i++) {
@@ -157,10 +207,10 @@ class Program {
         }
         return controlSumBits;
     }
-    private static List<BitArray> VerticalAndHorizontalParityControl(List<BitArray> bitMatrix) {
+    private static List<BitArray> VerticalAndHorizontalParityControl() {
         List<BitArray> resultBytes = new List<BitArray>();
-        VControlSumBits = GetControlSumVertical(bitMatrix);
-        HControlSumBits = GetControlSumHorizontal(bitMatrix);
+        VControlSumBits = GetControlSumVertical();
+        HControlSumBits = GetControlSumHorizontal();
         for (int i = 0; i < bitMatrix.Count; i++) {
             BitArray resultByte = new BitArray(bitMatrix[i].Length + 1);
             for (int j = 0; j < bitMatrix[i].Length; j++) {
@@ -172,7 +222,7 @@ class Program {
         resultBytes.Add(HControlSumBits);
         return resultBytes;
     }
-    private static BitArray GetControlSumHorizontal(List<BitArray> bitMatrix) {
+    private static BitArray GetControlSumHorizontal() {
         BitArray controlSumBits = new(bitMatrix[0].Count);
         int numRows = bitMatrix.Count;
         int numCols = bitMatrix[0].Count;
@@ -196,22 +246,7 @@ class Program {
         for (int i = 15; i >= 0; i--) {
             binaryValue += ((value >> i) & 1) == 1 ? "1" : "0";
         }
-        Console.WriteLine($"{binaryValue}");  // Output: 00010011}
-    }
-    static int CountFirstZeros(ushort number) {
-        int numZeros = 0;
-
-        // Count the number of zeros before the first one
-        for (ushort mask = 0x8000; mask > 0; mask >>= 1) {
-            if ((number & mask) == 0) {
-                numZeros++;
-            }
-            else {
-                break;
-            }
-        }
-
-        return numZeros;
+        Console.WriteLine($"{binaryValue}"); 
     }
 
 
@@ -224,7 +259,7 @@ class Program {
         }
         return mirrored;
     }
-    public static ushort MirrorBytesBySize(ushort value, int bitWidth) {
+    public static ushort MirrorBitsBySize(ushort value, int bitWidth) {
         ushort mirrored = 0;
         for (int i = 0; i < bitWidth; i++) {
             if ((value & (1 << i)) != 0) {
@@ -235,39 +270,44 @@ class Program {
     }
 
     static ushort CalculateCRC(byte[] bytes) {
-        ushort poly = 0x0805;           // setup example poly
-        ushort crcsize = 14;           // setup example crc width
-        ushort initialValue = 0;       // setup example initial value
-        bool inputReflected = true;   // setup example input reflection
-        bool resultReflected = true;  // setup example result reflection
-        ushort finalXor = 0;           // setup example final XOR value
+        ushort poly = 0x0805;         
+        ushort crcsize = 14;          
+        ushort initialValue = 0;      
+        bool inputReflected = true;   
+        bool resultReflected = true;  
+        ushort finalXor = 0;           
 
-        ushort bitsToShift = (ushort)(crcsize - 8);       // example CRC-11: 3
-        ushort bitmask = (ushort)(1 << (crcsize - 1));    // example CRC-11: 0x0400
-        ushort finalmask = (ushort)((1 << crcsize) - 1);  // example CRC-11: 0x07FF
+        ushort bitsToShift = (ushort)(crcsize - 8);       
+        ushort bitmask = (ushort)(1 << (crcsize - 1));    
+        ushort finalmask = (ushort)((1 << crcsize) - 1);  
 
-        ushort crc = initialValue;
+        crc = initialValue;
 
         foreach (byte b in bytes) {
-            byte curByte = (inputReflected ? Mirror8Bits(b) : b); ;
-            crc ^= (ushort)(curByte << bitsToShift); /* move byte into MSB of CRC */
-
+            byte currByte = (inputReflected ? Mirror8Bits(b) : b);
+            //Console.WriteLine($"processing byte: {b :X2}");
+            crc ^= (ushort)(currByte << bitsToShift);
             for (int i = 0; i < 8; i++) {
+            //PrintBinary(crc);
+                
                 if ((crc & bitmask) != 0) {
+                    //Console.WriteLine("XOR: ");
+                    //PrintBinary(poly);
                     crc = (ushort)(((crc << 1) ^ poly));
                 }
                 else {
                     crc <<= 1;
                 }
+                //Console.WriteLine("");
             }
 
             crc = (ushort)(crc & finalmask);
         }
 
-        crc = (ushort)(resultReflected ? MirrorBytesBySize(crc, crcsize) : crc);
+        crc = (ushort)(resultReflected ? MirrorBitsBySize(crc, crcsize) : crc);
         crc ^= finalXor;
 
-        PrintBinary(crc);
+        //PrintBinary(crc);
         return (ushort)(crc);
     }
 }
